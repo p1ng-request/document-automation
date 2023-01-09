6ded4f5b94610e1d1c8f683e680228435a2149e8
p1ng-request <117622198+p1ng-request@users.noreply.github.com>
2023-01-09 14:09:03 +0800

    Update check-dead-links.py
    Modified the crawl_site function to use a queue data structure to store the URLs to be crawled, and a set data structure to store the URLs that have already been crawled.

--- check-dead-links.py
+++ check-dead-links.py
@@ -1,11 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
+from collections import deque
+
 
 def check_link(url, base_url):
   if url.startswith('http'):
     try:
       r = requests.get(url)
+      if r.status_code == 200:
+        return True
+      else:
+        return False
+    except:
+      return False
+  elif '#' in url:
+    try:
+      r = requests.get(base_url)
       if r.status_code == 200:
         return True
       else:
@@ -34,15 +45,28 @@ def find_dead_links(url, base_url):
   return dead_links
 
 def crawl_site(url):
-  r = requests.get(url)
-  soup = BeautifulSoup(r.text, 'html.parser')
-  links = soup.find_all('a')
-  for link in links:
-    href = link.get('href')
-    if href.startswith(url):
-      dead_links = find_dead_links(href, url)
-      for link, page in dead_links:
-        print(f'Dead link {link} found on page {page}')
+  queue = deque([url])
+  visited = set()
+  dead_links = []
+  while queue:
+    current_url = queue.popleft()
+    r = requests.get(current_url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.find_all('a')
+    for link in links:
+      href = link.get('href')
+      if not check_link(href, url):
+        dead_links.append((href, current_url))
+    for link in links:
+      href = link.get('href')
+      if href.startswith(url) and href not in visited:
+        queue.append(href)
+        visited.add(href)
+  if not dead_links:
+    print('No broken links found!')
+  else:
+    for link, page in dead_links:
+      print(f'Dead link {link} found on page {page}')
 
 url = 'https://docs.kanaries.net'
 crawl_site(url)
