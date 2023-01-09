eaa70572143b5384e42f21abf7a1a9fe722c6ee0
p1ng-request <117622198+p1ng-request@users.noreply.github.com>
2023-01-09 12:13:55 +0800

    Update check-dead-links.py
    handle relative links without making false positives

--- check-dead-links.py
+++ check-dead-links.py
@@ -1,24 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-def check_link(url):
-  try:
-    r = requests.get(url)
-    if r.status_code == 200:
-      return True
-    else:
+def check_link(url, base_url):
+  if url.startswith('http'):
+    try:
+      r = requests.get(url)
+      if r.status_code == 200:
+        return True
+      else:
+        return False
+    except:
       return False
-  except:
-    return False
+  else:
+    try:
+      r = requests.get(base_url + url)
+      if r.status_code == 200:
+        return True
+      else:
+        return False
+    except:
+      return False
 
-def find_dead_links(url):
+def find_dead_links(url, base_url):
   r = requests.get(url)
   soup = BeautifulSoup(r.text, 'html.parser')
   links = soup.find_all('a')
   dead_links = []
   for link in links:
     href = link.get('href')
-    if not check_link(href):
+    if not check_link(href, base_url):
       dead_links.append((href, url))
   return dead_links
 
@@ -29,7 +39,7 @@ def crawl_site(url):
   for link in links:
     href = link.get('href')
     if href.startswith(url):
-      dead_links = find_dead_links(href)
+      dead_links = find_dead_links(href, url)
       for link, page in dead_links:
         print(f'Dead link {link} found on page {page}')
 
