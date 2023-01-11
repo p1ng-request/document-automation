90e04b1b5beb2f9b5366f21370db7a504f00ceae
p1ng-request <117622198+p1ng-request@users.noreply.github.com>
2023-01-11 23:42:15 +0800

    Update check-dead-links.py

--- check-dead-links.py
+++ check-dead-links.py
@@ -3,70 +3,52 @@ from bs4 import BeautifulSoup
 import urllib.parse
 from collections import deque
 
+session = requests.Session()
 
 def check_link(url, base_url):
-  if url.startswith('http'):
     try:
-      r = requests.get(url)
-      if r.status_code == 200:
-        return True
-      else:
-        return False
+        if url.startswith("#"):
+            url = urllib.parse.urljoin(base_url, url)
+        r = session.head(url, allow_redirects=True)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
     except:
-      return False
-  elif '#' in url:
-    try:
-      r = requests.get(base_url)
-      if r.status_code == 200:
-        return True
-      else:
         return False
-    except:
-      return False
-  else:
-    try:
-      r = requests.get(urllib.parse.urljoin(base_url, url))
-      if r.status_code == 200:
-        return True
-      else:
-        return False
-    except:
-      return False
-
-def find_dead_links(url, base_url):
-  r = requests.get(url)
-  soup = BeautifulSoup(r.text, 'html.parser')
-  links = soup.find_all('a')
-  dead_links = []
-  for link in links:
-    href = link.get('href')
-    if not check_link(href, base_url):
-      dead_links.append((href, url))
-  return dead_links
-
 def crawl_site(url):
-  queue = deque([url])
-  visited = set()
-  dead_links = []
-  while queue:
-    current_url = queue.popleft()
-    r = requests.get(current_url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    links = soup.find_all('a')
-    for link in links:
-      href = link.get('href')
-      if not check_link(href, url):
-        dead_links.append((href, current_url))
-    for link in links:
-      href = link.get('href')
-      if href.startswith(url) and href not in visited:
-        queue.append(href)
-        visited.add(href)
-  if not dead_links:
-    print('No dead links found!')
-  else:
-    for link, page in dead_links:
-      print(f'Dead link {link} found on page {page}')
+    queue = deque([url])
+    visited = set()
+    dead_links = []
+    session = requests.Session()
+    while queue:
+        current_url = queue.popleft()
+        parsed_url = urllib.parse.urlparse(current_url)
+        try:
+            r = session.get(current_url)
+        except requests.exceptions.RequestException as e:
+            # Log the error for debugging purposes
+            print(f'Error: {e}')
+            continue
+        soup = BeautifulSoup(r.text, 'html.parser')
+        links = soup.find_all('a')
+        for link in links:
+            href = link.get('href')
+            if href.startswith('/'):
+                href = urllib.parse.urljoin(current_url, href)
+            elif href.startswith('http'):
+                pass
+            else:
+                continue 
+            if not check_link(href, parsed_url):
+                dead_links.append((href, current_url))
+                print(f'Dead link {href} found on page {current_url}')
+        for link in links:
+            href = link.get('href')
+            if parsed_url.hostname == urllib.parse.urlparse(href).hostname and href not in visited:
+                queue.append(href)
+                visited.add(href)
+    session.close()
 
-url = 'https://url'
+url = 'https://url/'
 crawl_site(url)
