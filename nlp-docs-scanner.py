b38a55a81f28e843d6ae4bb9d6bbfadebd686d9d
p1ng-request <117622198+p1ng-request@users.noreply.github.com>
2023-01-12 11:28:47 +0800

    Update nlp-docs-scanner.py

--- nlp-docs-scanner.py
+++ nlp-docs-scanner.py
@@ -7,32 +7,22 @@ from pathlib import Path
 import markdown
 from nltk.sentiment import SentimentIntensityAnalyzer
 from language_tool_python import LanguageTool
-import textblob as tb
 
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 
 def clean_html(documentation: str) -> str:
-    """Removes any HTML tags from the documentation
-    :param documentation: str
-    :return: str
-    """
+    # Removes any HTML tags from the documentation
     cleanr = re.compile('<.*?>')
     cleantext = re.sub(cleanr, '', documentation)
     return cleantext
 
 def parse_markdown(documentation: str) -> str:
-    """Convert markdown to html
-    :param documentation: str
-    :return: str
-    """
+    # Convert markdown to html
     return markdown.markdown(documentation)
 
 def remove_header(documentation: str) -> str:
-    """Remove YAML front matter
-    :param documentation: str
-    :return: str
-    """
+    # Remove YAML front matter
     lines = documentation.split("\n")
     for i, line in enumerate(lines):
         if line.strip() == "---":
@@ -44,11 +34,14 @@ def remove_header(documentation: str) -> str:
             break
     return "\n".join(lines[end+1:])
 
+def remove_code_blocks(documentation: str) -> str:
+    # Removes any code blocks from the documentation
+    cleanr = re.compile('```.*?```|<.*?>')
+    cleantext = re.sub(cleanr, '', documentation, flags=re.DOTALL)
+    return cleantext
+
 def check_tone(documentation: str) -> str:
-    """Check for Tone in the text
-    :param documentation: str
-    :return: str
-    """
+    # Check for Tone in the text
     # Create an instance of the sentiment intensity analyzer
     sentiment_analyzer = SentimentIntensityAnalyzer()
     # Get the sentiment score of the documentation
@@ -65,21 +58,22 @@ def check_tone(documentation: str) -> str:
         return "The documentation is written in a very positive tone."
 
 def score_documentation(documentation: str) -> float:
-    """
-    Compute the Flesch-Kincaid readability test
-    """
+    # Compute the Flesch-Kincaid readability test
     fk_score = textstat.flesch_kincaid_grade(documentation)
     return fk_score
+
 
 def suggest_improvements(documentation: str) -> List[Tuple[str, str]]:
     suggestions = []
     # create an instance of the LanguageTool class
     tool = LanguageTool('en-US')
-    # Cehck with Grammarly
-    # grammarly.Client.check(documentation)
     # Compute the Flesch-Kincaid readability test
     fk_score = score_documentation(documentation)
     coleman_score = textstat.coleman_liau_index(documentation)
+    # Check for grammar and spelling errors
+    grammar_errors = tool.check(documentation)
+    if grammar_errors:
+        suggestions.append(("There are grammatical and/or spelling errors in the document. Consider running the document through a grammar and spell checker.", "Grammar and Spelling"))
     # Check if the documentation has an objective tone
     objective_score = textstat.automated_readability_index(documentation)
     clear_score = textstat.flesch_reading_ease(documentation)
@@ -97,21 +91,36 @@ def suggest_improvements(documentation: str) -> List[Tuple[str, str]]:
         suggestions.append(("The document appears to be written in a subjective tone.", "Objectivity"))
     if clear_score < 30:
         suggestions.append(("The document appears to be difficult to read. Consider simplifying the language.", "Readability"))
-        # Split the documentation into sentences
+    # Split the documentation into sentences
     sentences = nltk.sent_tokenize(documentation)
-    # Calculate Readability scores for each sentence
-    # for sentence in sentences:
-    #     score = textstat.flesch_reading_ease(sentence)
-    #     if score < 10:
-    #         suggestions.append("The sentence: '{}' has a low readability score of {}. Consider simplifying the language.".format(sentence, score))
+    for sentence in sentences:
+        score = textstat.flesch_reading_ease(sentence)
+        if score < 10:
+            suggestions.append("The sentence: '{}' has a low readability score of {}. Consider simplifying the language.".format(sentence, score))
         # Perform named entity recognition
         # entities = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentence)))
         # for entity in entities:
         #     if hasattr(entity, 'label'):
         #         if entity.label() == 'PERSON':
-        #             suggestions.append("The use of proper names such as "+str(entity)+" can sometimes be confusing and can be replaced with more general terms.")
+        #             suggestions.append("The use of proper names such as "+str(entity)+" can sometimes be confusing and can be replaced with more general terms.", "Clearity")
+    # Term/Terminology-Consistency checker
+    terms_dict = {}
+    words = nltk.word_tokenize(documentation)
+    # Collect a large data set of terms
+    for word in words:
+        if word in terms_dict:
+            terms_dict[word] += 1
+        else:
+            terms_dict[word] = 1
+    # Check the consistency of the terms used in each sentence
+    sentences = nltk.sent_tokenize(documentation)
+    for sentence in sentences:
+        words = nltk.word_tokenize(sentence)
+        for word in words:
+            if word not in terms_dict:
+                suggestions.append(f"The word '{word}' is not consistent with the terms found in the rest of the documentation.", "Consistency")
     return suggestions
-
+    
 def scan_documentation(file: str) -> float:
     # read the documentation file
     with open(file, 'r') as f:
@@ -159,9 +168,11 @@ def scan_all_documentations(root_dir: str):
     return scores
 
 def main():
-    root_dir = '/path'
+    root_dir = '/path/'
     scores = scan_all_documentations(root_dir)
     # print(scores)
 
 if __name__ == '__main__':
     main()
+
+
