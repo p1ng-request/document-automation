1ed7bc67271a1ae978977c683594f482c8857be5
p1ng-request <117622198+p1ng-request@users.noreply.github.com>
2023-01-15 21:32:53 +0800

    Update ml-docs-scanner.py
    removed defunct codes

--- ml-docs-scanner.py
+++ ml-docs-scanner.py
@@ -143,11 +143,4 @@ if __name__ == '__main__':
             for suggestion in suggestions:
                 print(f"{suggestion[0]} - {suggestion[1]}")
         documents.append(contents)
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(documents)
-    y = labels
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    clf = LinearSVC()
-    clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    print(f"Accuracy of the classifier: {accuracy_score(y_test, y_pred)}")
+ 
